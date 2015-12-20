9f917499711033901a50717595bbf3f7c26195d4
Andrés Leone <forestrf@gmail.com>
2015-12-20 04:22:29 +0100

    Recycle messages that the server doesn't send when calling SendToAll with only one client that we "except" in the call.

--- Lidgren.Network/NetServer.cs
+++ Lidgren.Network/NetServer.cs
@@ -63,6 +63,8 @@ namespace Lidgren.Network
 
 			if (recipients.Count > 0)
 				SendMessage(msg, recipients, method, sequenceChannel);
+			else if (msg.m_isSent == false)
+				Recycle(msg);
 		}
 
 		/// <summary>
